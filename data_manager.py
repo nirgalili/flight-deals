5bffeb960a49fec63f2e5f89ad98c10299c991e8
Nir Galili <nirgalili1@gmail.com>
2021-10-21 10:56:17 +0300

    put iataCode in sheety

--- data_manager.py
+++ data_manager.py
@@ -1,4 +1,20 @@
-class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+import requests
 
+update_iataCode_in_sheet_endpoint = "https://api.sheety.co/530a49192b31d09ac9d82f6490293373/myFlightDeals/prices/"
+
+
+class DataManager:
+
+    # This class is responsible for talking to the Google Sheet.
+
+    def __init__(self, row):
+        self.code = row["iataCode"]
+        self.id = row["id"]
+        self.json_row = {
+            "price": row
+        }
+
+        self.update_iatacode_in_sheety()
+
+    def update_iatacode_in_sheety(self):
+        requests.put(url=f"{update_iataCode_in_sheet_endpoint}{self.id}", json=self.json_row)
--- main.py
+++ main.py
@@ -1,26 +1,29 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 import requests
 from flight_search import FlightSearch
+from data_manager import DataManager
 
 wish_deals_endpoint = "https://api.sheety.co/530a49192b31d09ac9d82f6490293373/myFlightDeals/prices"
 
 response = requests.get(url=wish_deals_endpoint)
 response_data = response.json()
-print("____________________________________________________")
-print(response.text)
-print("____________________________________________________")
+# print("____________________________________________________")
+# print(response.text)
+# print("____________________________________________________")
 sheet_data = response_data["prices"]
-print(sheet_data)
+# print(sheet_data)
 
 
-for i in sheet_data:
-    iataCode = i["iataCode"]
+for row_in_sheety_table in sheet_data:
+    iataCode = row_in_sheety_table["iataCode"]
     if iataCode =="":
         my_flight_search = FlightSearch(iataCode)
         my_flight_search.get_update_iataCode()
         update_iataCode = my_flight_search.code
         # print(update_iataCode)
-        i["iataCode"] = update_iataCode
+        row_in_sheety_table["iataCode"] = update_iataCode
+        my_data_manager = DataManager(row_in_sheety_table)
 
-print("____________________________________________________")
-print(sheet_data)
+
+# print("____________________________________________________")
+# print(sheet_data)
